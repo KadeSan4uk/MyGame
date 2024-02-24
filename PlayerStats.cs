176b325542f722f4d204552fb2e3675159a3549d
KadeSan4uk <kotejchik@gmail.com>
2024-02-24 21:28:32 +0200

    start

---
+++ PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace MyGame
+{
+    public class PlayerStats
+    {
+        private string _name;
+        private string _race;
+        public PlayerStats(string name, string race)
+        {
+            _name=name;
+            _race=race;
+        }
+        
+    }
+}
--- StartGame.cs
+++ StartGame.cs
@@ -1,9 +1,50 @@
-﻿namespace MyGame
+﻿using System.Threading.Channels;
+
+namespace MyGame
 {
     public class StartGame
     {
         public static void Main(string[] args)
         {
+            string name;
+            string race;
+            
+            Console.WriteLine("Привет!");
+            Console.WriteLine("Введи имя персонажа");
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Выбери рассу:\n1: Human 2: Orc");
+            int flagPlayerClass = Convert.ToInt32(Console.ReadLine());
+            while(flagPlayerClass != 1||flagPlayerClass!=2) 
+            {
+                if (flagPlayerClass == 1)
+                {
+                  
+                    Console.WriteLine("Отлично ты выбрал Human!");
+                    break;
+                }
+                else if (flagPlayerClass == 2)
+                {
+                   
+                    Console.WriteLine("Отлично ты выбрал Orc!");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Попробуй еще...");
+                    Console.WriteLine("Выбери рассу:\n1: Human 2: Orc");
+                    flagPlayerClass = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+            
+
+
+
+
+
+
 
         }
     }
