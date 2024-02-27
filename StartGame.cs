52d2ad96b5f7fd175c4ccc617f5f836d866fa524
KadeSan4uk <kotejchik@gmail.com>
2024-02-27 11:50:34 +0200

    Добавлено: Игра с начала  Рост Уровня героя и урона

--- StartGame.cs
+++ StartGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Channels;
+﻿using System.Data;
+using System.Threading.Channels;
 
 namespace MyGame
 {
@@ -8,151 +9,33 @@ namespace MyGame
 
         public static void Main(string[] args)
         {
-            int playerHealth = 3;
+            int countPlayerHelth = 4;
+            int countExpereince = 0;
+            int globalExperience = 1;
+            int playerLevel = 1;
+            int playerExperience=0; 
+            int playerHealth = countPlayerHelth;            
+            int playerDamage = 1;
+            int enemyLevel = 1;
             int enemyHealth = 3;
-            int playerAttack = 1;
-            int enemyAttack = 1;
+            int enemyDamage = 1;
             bool fightState=false;
             bool enemy=false;
             int counterAction = 1;
             bool enemyFresh=false;
 
             while (true)
-            {
+            {               
                 Console.WriteLine();
-                Console.WriteLine($"  <=== ход {counterAction} ===>");
-                
+                Console.WriteLine($"  <=== ход {counterAction} ===>");                
+                                
                 fightState = enemy is true;
 
                 if (enemy)                
-                    enemyFresh = false;                  
-                                    
-                // a)
-                Console.WriteLine($" Состояние игрока: {(fightState? "в бою":"в покое")} || Жизни героя {playerHealth}");
-
-                if (!fightState) 
-                {
-                    Console.WriteLine($" Возможные действие:");
-                    Console.WriteLine($" 3) = искать врага");
-                }
-                else
-                {
-                    Console.WriteLine($" Возможные действие:");
-                    Console.WriteLine($" 1) = бить");
-                    Console.WriteLine($" 2) = сбежать");
-                }
-
-                // б)               
-                //if(!enemy)
-                //    Console.WriteLine($"3=искать врага");
-
-                Console.Write($" Выбрать действие:");
-                string action=Console.ReadLine();
-                
-
-                //в)
-                switch (action)
-                {
-                    case "1":
-                        if (enemy)
-                        {
-                            Console.Clear();
-                            
-                            int chance = random.Next(0, 100);
-
-                            if (chance > 10)
-                            {
-                                Console.WriteLine($" враг повержен");
-                                enemy = false; 
-                                
-                            }
-                            else
-                            {
-                                Console.WriteLine($" промах");
-                            }                            
-                        }
-                        else 
-                        {
-                            Console.WriteLine($" враг отсутсвует, ищем врага");
-                            int chance = random.Next(0, 100);
-
-                            if (chance > 50)
-                            {
-                                Console.WriteLine($" враг найден");
-                                enemy = true;
-                                enemyFresh = true;
-                                Console.WriteLine($" Жизни врага {enemyHealth}");
-                            }
-                            else
-                            {
-                                Console.WriteLine($" неудачный поиск врага");
-                            }
-                        }
-                        break;
-                    case "2":
-                        if (enemy)
-                        {
-                            Console.Clear();
-                            int chance = random.Next( 0, 100);
-                            if (chance > 10)
-                            {
-                                Console.WriteLine($" Герой сбежал от врага");
-                                enemy = false;
-                            }
-                            else
-                            {
-                                Console.WriteLine($" побег неудался");                                                               
-                            }                           
-                        }
-                        else
-                        {
-                            Console.WriteLine($" врага не найден");
-                        }
-                        break;
-                    case "3":
-                        if (enemy)
-                        {
-                            Console.Clear();
-                            Console.WriteLine($" враг уже есть, повторите действие");
-                            continue;
-                            
-                        }
-                        else
-                        {
-                            Console.Clear();
-
-                            int chance = random.Next(0, 100);
-
-                            if (chance > 50)
-                            {
-                                Console.WriteLine($" Результат поиска: Враг найден");
-                                enemy = true;
-                                enemyFresh = true;                                
-                            }
-                            else
-                            {
-                                Console.Clear();
-                                Console.WriteLine($" Результат поиска: Враг не найден");                                
-                            }                            
-                        }
-                        break;
-                }
-                //д)
-                if (enemy && enemyFresh==false)
-                {
-                    Console.WriteLine($" враг нанес удар");
-                    
-                    playerHealth -= 1;
-                    if (playerHealth == 0)
-                    {
-                        Console.WriteLine($" герой погиб.");
-                        Console.WriteLine($" Game Over!");
-                        break;
-                    }
-                }
+                    enemyFresh = false;
                 //г)
                 if (!enemy)
-                {                                   
+                {
                     int chance = random.Next(0, 100);
 
                     if (chance > 80)
@@ -163,9 +46,171 @@ namespace MyGame
                     else
                     {
                         Console.WriteLine($" В мире ничего не произошло");
-                    }              
+                    }                    
                 }               
-                counterAction++;          
+                // a)
+                Console.WriteLine($" Состояние игрока: {(fightState? "в бою  ":"в покое")} || Уровень героя {playerLevel}\n" +
+                    $"\t\t\t   || Жизни героя {playerHealth}");
+                Console.WriteLine();
+
+                if (enemy)
+                {
+                    Console.WriteLine($"\t\t\t   || Уровень врага {enemyLevel}");
+                    Console.WriteLine($"\t\t\t   || Жизни врага {enemyHealth}");
+                }
+                // б)
+                if (!enemy) 
+                {
+                    Console.WriteLine($" Возможные действие:");
+                    Console.WriteLine($" 3) = искать врага");
+                }
+                else
+                {
+                    Console.WriteLine($" Возможные действие:");
+                    Console.WriteLine($" 1) = атаковать");
+                    Console.WriteLine($" 2) = сбежать");
+                }
+                //в)
+                Console.Write($" Выбрать действие:");
+                string action = Console.ReadLine();
+                switch (action)
+                {
+                    case "1":
+                        if (enemy)
+                        {
+                            int chance = random.Next(0, 100);
+
+                            if (chance > 5)
+                            {                                
+                                if(enemyHealth>1)
+                                {
+                                    Console.WriteLine($" \t\t\t   |=> Герой нанес {playerDamage} урона");
+                                    enemyHealth -= playerDamage;
+                                }                             
+                                else if(enemyHealth <=1)
+                                {
+                                    Console.WriteLine($" \t\t\t   |=> Герой нанес сокрушительный удар");
+                                    Console.WriteLine($"\t\t\t    < Враг повержен! >");
+                                    Console.WriteLine($" Герой получил {globalExperience} опыта");
+                                    playerExperience += globalExperience;
+                                    countExpereince++;
+                                    if (countExpereince > 2)
+                                    {
+                                        playerLevel += 1;
+                                        Console.WriteLine($"\t\t\t Герой достиг {playerLevel} уровня! ");
+                                        playerDamage++;
+                                        countPlayerHelth++;
+                                        countExpereince = 0;
+                                    }
+                                    enemy = false;
+                                    enemyHealth = 3;
+                                    playerHealth = countPlayerHelth;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine($" Промах");                                
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($" Враг отсутсвует, ищем врага");
+                            int chance = random.Next(0, 100);
+
+                            if (chance > 20)
+                            {
+                                Console.WriteLine($" Враг найден!");
+                                enemy = true;
+                                enemyFresh = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine($" Неудачный поиск врага");
+                            }
+                        }
+                        break;
+                    case "2":
+                        if (enemy)
+                        {
+                            int chance = random.Next(0, 100);
+                            if (chance > 10)
+                            {
+                                Console.WriteLine($" Герой сбежал от врага");
+                                enemy = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine($" Побег неудался");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($" Врага не найден");
+                        }
+                        break;
+                    case "3":
+                        if (enemy)
+                        {
+                            Console.WriteLine($" Враг уже есть, повторите действие");
+                            continue;
+                        }
+                        else
+                        {
+                            int chance = random.Next(0, 100);
+
+                            if (chance > 20)
+                            {
+                                Console.WriteLine($" Результат поиска: Враг найден!");
+                                enemy = true;
+                                enemyFresh = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine($" Результат поиска: Враг не найден");
+                            }
+                        }
+                        break;
+                }
+                //д)
+                if (enemy && enemyFresh==false)
+                {
+                    if (enemyHealth > 0)
+                    {
+                        Console.WriteLine($"\t\t\t   |=> Враг нанес {enemyDamage} урона");
+                        playerHealth -= 1;
+                    }
+                    
+                    if (playerHealth <= 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($" Герой погиб!\t|| Начать заново?");                     
+                        Console.WriteLine($" Возможные действия:");
+                        Console.WriteLine($" 1) = Начать заново");
+                        Console.WriteLine($" 2) = Покинуть игру");
+
+                        string actionEsc=Console.ReadLine();
+
+                        switch (actionEsc)
+                        {
+                            case "1": counterAction = 1;
+                                Console.Clear();
+                                playerHealth = countPlayerHelth;
+                                enemyHealth = 3;
+                                countPlayerHelth = 4;
+                                playerExperience = 0;
+                                enemy = false;                                
+                                continue;
+
+                            case "2": Console.WriteLine($" Выход из игры");                                      
+                                return;
+
+                            default:                                
+                                    Console.WriteLine($" Выход из игры");                                    
+                                return;                              
+                        }                        
+                    }
+                }                
+                counterAction++;                
             }           
         }
     }
